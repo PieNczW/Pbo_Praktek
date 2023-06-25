0d51cf0ba4e68730813ae13580da3e44504b6ad2
PieNczW <127370958+PieNczW@users.noreply.github.com>
2023-06-25 15:43:37 +0700

    Add files via upload

---
+++ pbo_2201082004/src/farrell/controller/BukuController.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package farrell.controller;
+import farrell.model.*;
+import java.util.*;
+import farrell.view.*;
+import farrell.dao.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.*;
+/**
+ *
+ * @author HP
+ */
+public class BukuController {
+    formBuku view;
+    Buku buku;
+    BukuDao dao;
+
+    public BukuController(formBuku view) {
+        this.view = view;
+        dao = new BukuDaoImpl();
+    }
+    
+    public void clearForm(){
+        view.getTxtKodeBuku().setText("");
+        view.getTxtJudul().setText("");
+        view.getTxtPengarang().setText("");
+        view.getTxtPenerbit().setText("");
+    }
+    
+    public void tampil(){
+        DefaultTableModel tabelModel =(DefaultTableModel) view.getTabelBuku().getModel();
+        tabelModel.setRowCount(0);
+        List<Buku> list = dao.getAll();
+        for(Buku a : list){
+            Object[] row = {
+                a.getKodebuku(),
+                a.getJudul(),
+                a.getPengarang(),
+                a.getPenerbit()
+            };
+            tabelModel.addRow(row);
+        }
+    }
+    
+    public void insert(){
+        buku = new Buku();
+        buku.setKodebuku(view.getTxtKodeBuku().getText());
+        buku.setJudul(view.getTxtJudul().getText());
+        buku.setPengarang(view.getTxtPengarang().getText());
+        buku.setPenerbit(view.getTxtPenerbit().getText());
+        dao.insert(buku);
+        JOptionPane.showMessageDialog(view,"Entry Data Ok"); 
+    }
+    
+    public void update(){
+        int index = view.getTabelBuku().getSelectedRow();
+        buku.setKodebuku(view.getTxtKodeBuku().getText());
+        buku.setJudul(view.getTxtJudul().getText());
+        buku.setPengarang(view.getTxtPengarang().getText());
+        buku.setPenerbit(view.getTxtPenerbit().getText());
+        dao.update(index, buku);
+        JOptionPane.showMessageDialog(view,"Update Data Ok");
+    }
+    
+    public void delete(){
+        int index = view.getTabelBuku().getSelectedRow();
+        dao.delete(index);
+        JOptionPane.showMessageDialog(view,"Delete Data Ok");
+    }
+    
+    public void getBuku(){
+        int index = view.getTabelBuku().getSelectedRow();
+        buku = dao.getBuku(index);
+        view.getTxtKodeBuku().setText(buku.getKodebuku());
+        view.getTxtJudul().setText(buku.getJudul());
+        view.getTxtPengarang().setText(buku.getPengarang());
+        view.getTxtPenerbit().setText(buku.getPenerbit());
+        
+    }
+    
+    
+}
